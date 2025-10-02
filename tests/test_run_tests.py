bb4de29a8efa17248c08a54e98ac56ea1593c209
makasinod <makasin.od@gmail.com>
2025-10-02 17:58:11 +0300

    add new negative test for module1

--- tests/test_run_tests.py
+++ tests/test_run_tests.py
@@ -16,3 +16,7 @@ class TestRunModules:
     def test_module2_negative(self):
         mod2 = Module2()
         assert mod2.greet() != "Hello from Module 1!"
+
+    def test_module1_negative_by_text(self):
+        mod1 = Module1()
+        assert mod1.greet() != "Hello from Module 1 again!"
