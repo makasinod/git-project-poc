dc3500d085c98ecf20022fd316714f0d5f0e7c23
makasinod <makasin.od@gmail.com>
2025-10-02 18:33:25 +0300

    add changes from master

--- tests/test_run_tests.py
+++ tests/test_run_tests.py
@@ -2,7 +2,7 @@ from src import Module1, Module2
 
 class TestRunModules:
     def test_module1_greet(self, sample_fixture):
-        assert sample_fixture == "sample data1", f"Fixture did not yield expected data but: {sample_fixture}"
+        assert sample_fixture == "sample data", f"Fixture did not yield expected data but: {sample_fixture}"
         mod1 = Module1()
         assert mod1.greet() == "Hello from Module 1!"
 
