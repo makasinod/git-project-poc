27d59b3c4e467452b8dddf1e6880d8ec2c4f647e
makasinod <52178225+makasinod@users.noreply.github.com>
2025-10-02 17:55:12 +0300

    Update test_run_tests.py

--- tests/test_run_tests.py
+++ tests/test_run_tests.py
@@ -16,3 +16,7 @@ class TestRunModules:
     def test_module2_negative(self):
         mod2 = Module2()
         assert mod2.greet() != "Hello from Module 1!"
+
+    def test_module1_negative(self):
+        mod1 = Module1()
+        assert mod2.greet() != "Hello from Module 2!"
