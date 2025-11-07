fb0c67db5d2c6e713b5a6ff34c214ffe365fc646
makasinod <52178225+makasinod@users.noreply.github.com>
2025-11-07 19:07:33 +0200

    Add print statement in greet function
    Added a print statement at the start of the greet function.

--- src/module1.py
+++ src/module1.py
@@ -8,6 +8,7 @@ class Module1:
         self.name = "Module 1"
 
     def greet(self):
+        print("Start function greet")
         return f"Hello from {self.name}!"
     
 if __name__ == "__main__":
